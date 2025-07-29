03be8a32bc66f139cfc246110b834bee4dff7a32
Ali Shahrez <shahrez375@gmail.com>
2025-07-29 16:12:17 +0500

    Add functional grid size button

--- java.js
+++ java.js
@@ -1,8 +1,34 @@
 const container = document.querySelector(".container")
-const gridSize = 16
-const totalSquares = gridSize*gridSize
-for (let i = 0; i < totalSquares; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square')
-    container.appendChild(div)
+
+const gridSize = document.querySelector("#grid-size")
+const createButton = document.querySelector('#create-button')
+
+function makeGrid(size) {
+    container.innerHTML = '';
+    const squareSize=640/size
+    for (let i=0; i<size*size; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square')
+        div.style.width = `${squareSize}px`
+        div.style.height = `${squareSize}px`
+        container.appendChild(div)
+    }
 }
+function generateGrid() {
+    const size = parseInt(gridSize.value);
+    if (isNaN(size) || size<1 || size>100) {
+        alert("Please enter a valid number between 1 and 100.")
+        return;
+    }
+    makeGrid(size);
+}
+
+createButton.addEventListener('click', generateGrid)
+gridSize.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        generateGrid();
+    }
+})
+
+makeGrid(16)
+
